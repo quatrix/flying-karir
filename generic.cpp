b622eb8e86e19df48accb540d81f179eb6c2afaa
Eddie Mishelevich <evil.legacy@gmail.com>
2009-02-22 00:02:44 +0200

    added collision

---
+++ generic.cpp
@@ -0,0 +1,26 @@
+#include "generic.hpp"
+
+
+double assign_limited(double& x, double value, double limit) {
+	x += value;
+
+	if (x > limit) 
+		x = 0;
+	if (x < 0)
+		x = limit;
+
+	return x;
+}
+
+
+double return_limited(double value, double limit) {
+	if (value > limit) 
+		return limit;
+	else if (value < 0)
+		return 0;
+	else 
+		return value;
+}
+
+
+Cords::Cords(): x(0), y(0), degree(0) {}
